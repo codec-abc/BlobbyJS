d422d3fdc2c667043aa884395f9ec8232d0dc66a
codec-abc <noemail>
2017-01-07 14:05:35 +0100

    add global variable to be able to easily acces game state in debugging tool

--- src/Main.ts
+++ src/Main.ts
@@ -3,6 +3,7 @@
 import GameScreen = require('./views/screens/game/GameScreen');
 
 let gameScreen : GameScreen.GameScreen = new GameScreen.GameScreen();
+(<any> window).gameScreen = gameScreen; // set the game in a gameScreen property for debugging purpose
 var isRunningInElectron = (<any> window).isRunningInElectron;
 var modules = (<any> window).nodeModules;
 if(isRunningInElectron) {}
